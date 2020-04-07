c6b5faa429d087c92df1cca28192504c13352be0
Łukasz Zabuski <riraseth@gmail.com>
2020-04-08 00:38:52 +0200

    broken home page into components, finished styling menu item

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../components/directory/Directory';
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory></Directory>
+    </div>
+  );
+};
+
+export default Homepage;
