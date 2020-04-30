41ca0152eac044bce66f16162cf0727eaf0c5b8e
Łukasz Zabuski <riraseth@gmail.com>
2020-04-30 22:08:15 +0200

    updated custom button styles, added custom button to collectionc component

--- src/components/collection-item/CollectionItem.js
+++ src/components/collection-item/CollectionItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import CustomButton from '../custom-button/CustomButton';
 import './CollectionItem.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
@@ -13,6 +13,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted>Add to cart</CustomButton>
     </div>
   );
 };
