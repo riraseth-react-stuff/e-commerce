75b166c7e0f2cae8fa1c156a2b26e76168338f5f
Łukasz Zabuski <riraseth@gmail.com>
2020-05-14 01:03:37 +0200

    added collection routing and selector

---
+++ src/pages/Collection/CollectionPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectCollection } from '../../redux/shop/shop.selector';
+import CollectionItem from '../../components/collection-item/CollectionItem';
+import './CollectionPage.scss';
+const CollectionPage = ({ collection }) => {
+  return (
+    <div className="collection-page">
+      <h2>collection page</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
-const ShopPage = () => {
+import CollectionPage from '../Collection/CollectionPage';
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionsOverview></CollectionsOverview>
+      <Route exact path={`${match.path}`}>
+        <CollectionsOverview></CollectionsOverview>
+      </Route>
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      ></Route>
     </div>
   );
 };
 
-export default ShopPage;
+export default withRouter(ShopPage);
+
+// used component={} so don't have to use withRouter in CategoryPage
